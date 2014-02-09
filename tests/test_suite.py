9b6c554614adf05bae0e54132c48f4b5ae3904b8
Justin Noah <justinnoah@gmail.com>
2014-02-09 13:22:08 -0800

    tests: setUp and tearDown test db
    setUp and tearDown in the initial test suite now create a test database

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -1,12 +1,32 @@
+import os
 import unittest
+from datetime import datetime
 
-from pyvly.main import app
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from pyvly import database, main, models
 
 
 class PyVlyTestSuite(unittest.TestCase):
 
     def setUp(self):
-        pass
+        # Setup the testing environment
+        main.app.config['TESTING'] = True
+        self.app = main.app.test_client()
+
+        # Create a database for the tests
+        _basedir = os.path.abspath(os.path.dirname(__file__))
+        database_uri = "sqlite:///" + os.path.join(_basedir, "test.db")
+        self.engine = create_engine(database_uri)
+
+        self.db_session = scoped_session(sessionmaker(autocommit=False,
+                                                      autoflush=False,
+                                                      bind=self.engine))
+
+        database.Model.metadata.create_all(self.engine)
 
     def tearDown(self):
-        pass
+        # Clear the db
+        database.Model.metadata.drop_all(self.engine)
+        self.db_session.remove()
