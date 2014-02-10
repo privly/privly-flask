cf85b6eda2146f676a6ba10db4bdd941253e4935
spectralsun <garrett@spectralsun.com>
2014-02-09 17:16:43 -0800

    added extensible JSON encoder

--- pyvly/main.py
+++ pyvly/main.py
@@ -1,3 +1,13 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+class ExtensibleJSONEncoder(simplejson.JSONEncoder):
+    """A JSON encoder that will check for a .__json__ method on objects."""
+    def default(self, obj):
+       if hasattr(obj, '__json__'):
+          return obj.__json__()
+    return super(ExtensibleJSONEncoder, self).default(obj)
+
+app.json_encoder = ExtensibleJSONEncoder
+
