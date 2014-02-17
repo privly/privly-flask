e47c3c86f978fae75f9e07db46d4b0c5aaceefe8
spectralsun <garrett@spectralsun.com>
2014-02-17 15:53:52 -0800

    register posts

--- pyvly/main.py
+++ pyvly/main.py
@@ -1,13 +1,22 @@
+import simplejson
 from flask import Flask
 
+
+from pyvly.api import post
+
+
 app = Flask(__name__)
+
+
+app.register_blueprint(post.bp, '/posts')
 
 class ExtensibleJSONEncoder(simplejson.JSONEncoder):
     """A JSON encoder that will check for a .__json__ method on objects."""
     def default(self, obj):
-       if hasattr(obj, '__json__'):
-          return obj.__json__()
-    return super(ExtensibleJSONEncoder, self).default(obj)
+        if hasattr(obj, '__json__'):
+            return obj.__json__()
+        return super(ExtensibleJSONEncoder, self).default(obj)
+
 
 app.json_encoder = ExtensibleJSONEncoder
 
