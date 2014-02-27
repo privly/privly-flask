55d52c1543ff80c16259cbe5431067b54746b904
Justin Noah <justinnoah@gmail.com>
2014-02-26 20:09:04 -0800

    manage: add create_user command

--- manage.py
+++ manage.py
@@ -1,8 +1,16 @@
 from flask.ext.script import Manager
 
+from pyvly.helpers import create_user as cu
 from pyvly.main import app
 
 manager = Manager(app)
+
+@manager.command
+@manager.option('-e', '--email', required=True, help="User's email address")
+@manager.option('-p', '--password', required=True, help="User's password")
+def cerate_user(email, password):
+    cu(email, password)
+
 
 if __name__ == '__main__':
     manager.run()
