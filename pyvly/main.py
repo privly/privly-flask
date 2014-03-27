f94035dc795e5111efed5c6fbd373ff15b30ee9d
spectralsun <garrett@spectralsun.com>
2014-03-26 22:21:08 -0700

    mount user controller

--- pyvly/main.py
+++ pyvly/main.py
@@ -2,13 +2,14 @@ import simplejson
 from flask import Flask
 
 
-from pyvly.api import post
+from pyvly.controllers import post, user
 
 
 app = Flask(__name__)
 
 
 app.register_blueprint(post.bp, url_prefix='/posts')
+app.register_blueprint(user.bp, url_prefix='/user')
 
 class ExtensibleJSONEncoder(simplejson.JSONEncoder):
     """A JSON encoder that will check for a .__json__ method on objects."""
